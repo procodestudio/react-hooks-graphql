91f9a2c0b78d68e4b2bd373d62917e311dd7196f
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2020-05-20 14:34:04 -0300

    Convert stateless function to component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">Welcome to our app</div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [
+        { id: 1, name: 'Paula' },
+        { id: 2, name: 'Joseph' },
+        { id: 3, name: 'Mike' },
+        { id: 4, name: 'Albert' },
+      ],
+    };
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className="App">
+        { users.map((user) => <h1 className="username" key={user.id}>{user.name}</h1>) }
+      </div>
+    );
+  }
 }
 
 export default App;
